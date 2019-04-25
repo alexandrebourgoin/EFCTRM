6791f2e75a88c17cb82eb2928d447b94a9af4abb
Alexandre Bourgoin <alexandre.bourgoin@bradyplc.com>
2019-04-25 19:15:01 +0200

    v0.1

---
+++ DataModel1/EFCTRMDataModel.cs
@@ -0,0 +1,15 @@
+﻿using Business;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EFCTRMDataModel
+{
+    public class GeneralSetup : DbContext
+    {
+        public DbSet<Market> Markets { get; set; }
+    }
+}
